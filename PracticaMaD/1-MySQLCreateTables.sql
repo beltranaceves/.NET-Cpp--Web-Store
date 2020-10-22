882688ee65c9560a3903988a9a01f17fa202e828
Diego <diego.villanueva@udc.es>
2020-10-22 08:32:43 +0200

    Mas inserts añadidos para probar  después

--- PracticaMaD/1-MySQLCreateTables.sql
+++ PracticaMaD/1-MySQLCreateTables.sql
@@ -53,18 +53,29 @@ CREATE TABLE Product (
 	price float NOT NULL,
 	registerDate DATETIME NOT NULL,
 	stock smallint NOT NULL,
+	CONSTRAINT CategoryFK FOREIGN KEY (productCategory)
+		REFERENCES Category (name),
     CONSTRAINT ProductPK PRIMARY KEY (id);
     
 ) ENGINE = InnoDB;
 
 
 CREATE TABLE Category (
-	id BIGINT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(20) NOT NULL,
-    CONSTRAINT CategoryPK PRIMARY KEY (id);
+    CONSTRAINT CategoryPK PRIMARY KEY (name);
     
 ) ENGINE = InnoDB;
 
 INSERT INTO User (login,password,name,firstName,lastName,address,email, languaje) VALUES ('DigoStorm','1234','Diego','Villanueva','Farina','Mi Casa','d@udc.es','Español');
 INSERT INTO User (login,password,name,firstName,lastName,address,email, languaje) VALUES ('Jacojh21','1234','Jacobo','Jorge','Hermida','Su Casa','j@udc.es','Español');
+
+INSERT INTO CreditCard (number,type,name,verificationCode,expeditionDate) VALUES ('123456789','VISA',888,(DATE_ADD(DATE(NOW()), INTERVAL '0 20:55' DAY_MINUTE)));
+
+INSERT INTO Order (creditCardNumber,clientAdress,orderDate) VALUES ('123456789','Mi Casa',(DATE_ADD(DATE(NOW()), INTERVAL '0 20:55' DAY_MINUTE)));
+
+INSERT INTO Category (name) VALUES ('Sillas');
+
+
+INSERT INTO Product (name,price,registerDate,stock,productCategory) VALUES ('Silla Gamming',999,(DATE_ADD(DATE(NOW()), INTERVAL '0 20:55' DAY_MINUTE)),9,'Sillas');
+
 
